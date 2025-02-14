58eec680a2320f0b06b7b7ff68c5e736690e991c
Robbie Lumpkin <jrlumpkinjr@gmail.com>
2025-02-14 16:35:38 -0500

    first commit

---
+++ M1 Project - To Do Application.py
@@ -0,0 +1,15 @@
+print("\nWelcome to Module 1's Project: Use this To Do Application to organize your tasks today!")
+
+# build a CLI that 
+
+# welcomes users and display a menu with options
+    # to add tasks
+    # to view tasks
+    # to delete tasks
+    # to quit the application
+
+# tasks should be stored in a list 
+
+tasks = []
+
+input() 
